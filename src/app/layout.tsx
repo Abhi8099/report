248f008041ae0d7b96c4a523e3440b1a99c7f047
Abhi8099 <abhishek.sharma1@digranknow.com>
2024-10-28 17:50:56 +0530

    api uptime

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,9 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
+      </head>
       <body suppressHydrationWarning={true}>
 <Providers>
 { children}
