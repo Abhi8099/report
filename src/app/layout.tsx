8b6113bdd3ba0446c8d5935bd99eaa981c24f3e4
Abhi8099 <abhishek.sharma1@digranknow.com>
2024-10-28 18:08:39 +0530

    ..

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
+
       </head>
       <body suppressHydrationWarning={true}>
 <Providers>
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,2 +1,2 @@
 // utils/api.js
-export const BASE_URL = "http://64.227.171.184:8001/";
+export const BASE_URL = "http://64.227.171.184:8001/api/";
