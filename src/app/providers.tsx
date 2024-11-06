74a15e331762902dba517390cfd03c64e7a638a5
Abhi8099 <abhishek.sharma1@digranknow.com>
2024-11-06 13:47:45 +0530

    ...

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -7,30 +7,56 @@ import { Toaster } from 'react-hot-toast';
 import { GoogleAnalyticsDataProvider } from "@/helpers/GoogleAnalyticsDataContext";
 import { ProjectProvider } from "@/helpers/ProjectContext";
 import { ProfileProvider } from "@/helpers/ProfileContext";
-import React , { useRef } from 'react'
+import React , { useEffect, useRef } from 'react'
 import gsap from 'gsap-trial/dist/gsap';
 import { ScrollTrigger } from 'gsap-trial/dist/ScrollTrigger';
 import { ScrollSmoother } from 'gsap-trial/dist/ScrollSmoother';
 import useIsomorphicLayoutEffect from '../helpers/isomorphicEffect';
+import Lenis from '@studio-freight/lenis';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  const smoother:any = useRef();
-  const ctx:any = useRef();
+  // const smoother:any = useRef();
+  // const ctx:any = useRef();
+  // const pathname = usePathname();
+
+  // useIsomorphicLayoutEffect(() => {
+  //   gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+  //   ctx.current = gsap.context(() => {
+  //     smoother.current = ScrollSmoother.create({
+  //       smooth: 2,
+  //       effects: true,
+  //     });
+  //   });
+
+  //   return () => ctx.current.revert();
+  // }, [pathname]);
+
+
   const pathname = usePathname();
 
-  useIsomorphicLayoutEffect(() => {
-    gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      orientation: 'vertical',
+      gestureOrientation: 'vertical',
+      smoothWheel: true,
+      touchMultiplier: 2,
+    })
 
-    ctx.current = gsap.context(() => {
-      smoother.current = ScrollSmoother.create({
-        smooth: 2,
-        effects: true,
-      });
-    });
+    function raf(time: number) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
 
-    return () => ctx.current.revert();
-  }, [pathname]);
+    requestAnimationFrame(raf)
 
+    return () => {
+      lenis.destroy()
+    }
+  }, [pathname])
+  
 
   return <>  <SessionProvider>
     <Toaster />
@@ -38,11 +64,9 @@ export default function Providers({ children }: { children: React.ReactNode }) {
       <ProjectProvider>
         <GoogleAnalyticsDataProvider>
           <GoogleSearchConsoleDataProvider>
-          <div id="smooth-wrapper">
-          <div id="smooth-content">
+
             {children}
-            </div>
-            </div>
+
           </GoogleSearchConsoleDataProvider>
         </GoogleAnalyticsDataProvider>
       </ProjectProvider>
